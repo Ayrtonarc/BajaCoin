0e05bf8bbf9a0aa535d4aba1d80fa6c7b7505f5d
Ayrton <ayrton.arc@gmail.com>
2020-05-02 19:51:37 -0700

    Minando Block

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const {name, version} = PKG;
 
 console.log('${name} v${version}'); //bloque genesis
 const { genesis } = Block;
-console.log(genesis.toString());
 
-const block = new Block (Date.now(), genesis.hash, 'hash', 'date');  //bloque 1 conreferencia al bloque genesis
-console.log(block.toString());
+const block1 = Block.mine(genesis, 'data-1');
+console.log(block1.toString());
 
-const block_2 = new Block(Date.now(), block.hash, 'hash-2', 'date-2'); //bloque 2 con referenciaal 1
-console.log(block_2.toString());
+const block2 = Block.mine(block1, 'data-2');
+console.log(block2.toString());
+
--- src/blockchain/block.js
+++ src/blockchain/block.js
@@ -10,12 +10,20 @@ class Block{
         const timestamp = (new Date(2020, 0, 1)).getTime();
         return new this(timestamp, undefined, 'Genezi-hash', 'zero');
     }
-    //metodo Tostring para devolver inf almacenada en el bloque
+
+    static mine(previousBlock, data){
+        const timestamp = Date.now();
+        const hash = '@TODO-hash';
+        const { hash: previousHash } = previousBlock;
+
+        return new this(timestamp, previousHash, hash, data);
+    }
+                                                                                        //metodo Tostring para devolver inf almacenada en el bloque
     toString(){
         const{
             timestamp, previousHash, hash, data,
         } = this;
-                                                        //cadena de texto
+                                                                                         //cadena de texto
         return `Block -                          
         timestamp    : ${timestamp}
         previousHash : ${previousHash}
